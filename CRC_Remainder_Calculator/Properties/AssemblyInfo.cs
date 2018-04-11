2b874c31455f40615df1531ef90c26b11eb7e10a
Daniel Schefer <Daniel Schefer@DESKTOP-79LBSG3>
2018-04-11 18:40:07 +0300

    Initial Release

--- CRC_Remainder_Calculator/Properties/AssemblyInfo.cs
+++ CRC_Remainder_Calculator/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp14")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("CRC Remainder Calculator")]
+[assembly: AssemblyDescription("CRC Remainder Calculator")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp14")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCompany("Daniel Schafer")]
+[assembly: AssemblyProduct("CRC Remainder Calculator")]
+[assembly: AssemblyCopyright("Daniel Schafer ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
